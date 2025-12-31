e10db04fae4aab80734a3c7608f32ca0301c30ad
Bharat <cb_bharat@redtag.ae>
2025-12-31 11:58:07 +0530

    code push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
     setShowJourneyButton(true);
   };
 
-  if (false) {
+  if (showTimer) {
     return <Timer targetDate={targetDate} onComplete={handleTimerComplete} />;
   }
 
